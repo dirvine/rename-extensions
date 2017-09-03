f6485721a97f46612f3f1eecd2941bdc8af62d10
David Irvine <david.irvine@maidsafe.net>
2017-09-03 21:58:17 +0100

    inital commit

---
+++ src/main.rs
@@ -0,0 +1,61 @@
+extern crate clap;
+extern crate glob;
+
+
+use clap::{App, Arg};
+use glob::glob;
+use std::convert::From;
+use std::fs;
+
+
+fn main() {
+    let matches = App::new("rename file extensions")
+        .version("0.1")
+        .author("D. Irvine <david.irvine@maidsafe.net>")
+        .about("Rename file exensions (includes subdirectories)")
+        .arg(
+            Arg::with_name("start_dir")
+                .help(
+                    "path to the top level directory containing the files to rename",
+                )
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("from")
+                .help("Change extension from")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("to")
+                .help("Change extension to")
+                .takes_value(true)
+                .required(true),
+        )
+        .get_matches();
+
+    let start_dir = matches.value_of("start_dir").unwrap();
+    let from = matches.value_of("from").unwrap();
+    let to = matches.value_of("to").unwrap();
+
+    let search = String::from(start_dir) + "/**/*." + from;
+    let paths = glob(&search).expect("Could not find path given.").map(
+        |p| {
+            p.expect("Bad path name")
+        },
+    );
+
+    for path in paths {
+        match fs::rename(&path, &path.with_extension(to)) {
+            Ok(_) => {
+                println!(
+                    "Renamed :  {:?} to {:?}",
+                    path.clone(),
+                    &path.with_extension(to)
+                )
+            }
+            Err(msg) => println!("{}", msg),
+        };
+    }
+}
